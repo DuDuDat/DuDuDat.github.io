7733cce83aabf7b220d9a023937011c4c00ca1dd
DuDuDat <kjm1709@naver.com>
2024-05-18 21:29:52 +0900

    전체 수정 전 세이브 플러그인을 사용한 데이터 가공

--- _plugins/jekyll-collection-subdirs.rb
+++ _plugins/jekyll-collection-subdirs.rb
@@ -5,45 +5,77 @@ module Jekyll
   class CollectionSubdirsGenerator < Generator
     safe true
 
-    # def generate(site)
-    #   # site.config['test'] = 'Hello, Jekyll!'
-    #   values = []
-    #
-    #   site.collections.each do |label, collection|
-    #
-    #   end
-    # end
-
     def generate(site)
       collections = []
 
       site.collections.each do |label, collection|
-        collection_dir = File.join(site.source, collection.relative_directory)
+        collection_dir = File.join(site.source, collection.relative_directory) # _Develop
+        next if collection.relative_directory == "_posts" # _posts 제외
+        
+        dirs = search_directories(collection_dir)
 
-        if Dir.exist?(collection_dir)
-          subdirs = []
+        dirs.each do |dir| # Language
+          dir_path = File.join(collection_dir, dir)
 
-          Dir.foreach(collection_dir) do |entry|
-            next if entry == '.' || entry == '..'
-            full_path = File.join(collection_dir, entry)
-
-            if File.directory?(full_path)
-              puts "디렉토리임: #{entry}"
-              subdirs << entry
-            else
-              puts "파일임 : #{entry}"
-            end
+          # 파일 경로 예시: _Develop/Language/C
+          search_directories(dir_path).each do |sub_dir|
+            set_docs(site, collection.docs, File.join(dir_path, sub_dir))
           end
+          
+          # 파일 경로 예시 _Develop/Error 모음
+          set_docs(site, collection.docs, dir_path) # docs 정보
+        end
 
-          dict = {
-            "label" => collection.metadata["name"] || label, "subdirs" => subdirs,
+        # 컬렉션 정보 저장
+        if Dir.exist?(collection_dir) # collection_dir: D:/dududat.github.io/_Develop
+          collection_dict = {
+            "label" => collection.metadata["name"] || label, "dirs" => dirs, "img" => collection.metadata["img"]
           }
-          collections << dict
+          collections << collection_dict
+        end
+
+        # 디렉토리 아이콘 등 저장
+        json_file_path = File.join(site.source, 'directories_info.json')
+        if File.exist?(json_file_path)
+          json_data = File.read(json_file_path)
+          json_data = JSON.parse(json_data)
+          for dir_name, dics in json_data
+            dics['name'] = dics.fetch('name', dir_name)
+          end
+          site.config['directories_info'] = json_data
         end
       end
-      puts "#{collections}"
       site.config['test'] = collections
     end
+
+    # path: 디렉토리 경로, check: "dir" or "file" return sub_directories or sub_files
+    def search_directories(path)
+      sub_directories = []
+      if Dir.exist?(path)
+        Dir.foreach(path) do |entry|
+          next if entry == '.' || entry == '..'
+          full_path = File.join(path, entry)
+          if File.directory?(full_path)
+            sub_directories << entry
+          end
+        end
+      end
+      sub_directories
+    end
+
+    def set_docs(site, docs, path)
+      return_docs = []
+      val_path = path + "/"
+      docs.each do |doc|
+        if doc.path.split(val_path).last.include?("/")
+          doc = { "url" => doc.url, "title" => doc.data['title'], "content" => doc.content }
+          return_docs << doc
+        end
+      end
+      site.config[path.split("/").last] = return_docs # 폴더명: documents 정보
+    end
+
+
 
   end
 end
